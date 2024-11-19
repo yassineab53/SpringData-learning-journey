32c7ae3b99a50938b27d58aff5860a74722f6f70
yassine4real <yssnabbou2002@gmail.com>
2024-11-19 02:30:27 +0100

    a

--- src/main/java/net/yassine/university/UniversityApplication.java
+++ src/main/java/net/yassine/university/UniversityApplication.java
@@ -1,21 +1,24 @@
 package net.yassine.university;
 
+import net.yassine.university.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import net.yassine.university.entities.Product;
 
 @SpringBootApplication
 public class UniversityApplication implements CommandLineRunner {
-
+    @Autowired // pour l'injection des depandances
+    private ProductRepository productRepository;
     public static void main(String[] args) {
-
         SpringApplication.run(UniversityApplication.class, args);
-
-        System.out.println("hello world");
     }
 
     @Override
     public void run(String... args) throws Exception {
-
+        productRepository.save(new Product(null, "Computer", 4300, 3));
+        productRepository.save(new Product(null, "Printer", 1200, 4));
+        productRepository.save(new Product(null, "Smart Phone", 1300, 7));
     }
 }
--- src/main/java/net/yassine/university/repository/ProductRepository.java
+++ src/main/java/net/yassine/university/repository/ProductRepository.java
@@ -1,7 +1,10 @@
 package net.yassine.university.repository;
 
+import net.yassine.university.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 /**
  * @author $ {USER}
  **/
-public interface ProductRepository {
+public interface ProductRepository extends JpaRepository<Product, Long> {
 }
